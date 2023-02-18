f8d21344a884f94083a747ca0ebfb03af9c45f40
rituandhare <rituandhare77@gmail.com>
2023-02-18 18:46:52 +0530

    first commit

---
+++ src/main/java/com/example/demo/BslApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BslApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/BSLController.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller;
+
+public class BSLController {
+	
+	public String getName() {
+		return "my first controller";
+		
+	}
+
+}
